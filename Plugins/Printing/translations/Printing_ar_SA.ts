96bdf800bf2ff13adf8b772466ab9ff10f723506
Paweł Salawa <pawelsalawa@gmail.com>
2026-06-11 14:26:44 +0200

    New translations printing.ts (Arabic)
    [ci skip]

--- Plugins/Printing/translations/Printing_ar_SA.ts
+++ Plugins/Printing/translations/Printing_ar_SA.ts
@@ -1,40 +1,40 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ar" sourcelanguage="en">
-<context>
+  <context>
     <name>Printing</name>
     <message>
-        <location filename="../printing.cpp" line="35"/>
-        <source>Print data</source>
-        <translation type="unfinished">Print data</translation>
+      <location filename="../printing.cpp" line="35"/>
+      <source>Print data</source>
+      <translation>طباعة البيانات</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="37"/>
-        <source>Print query</source>
-        <translation type="unfinished">Print query</translation>
+      <location filename="../printing.cpp" line="37"/>
+      <source>Print query</source>
+      <translation>طباعة الاستعلام</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="74"/>
-        <source>No data to print.</source>
-        <translation type="unfinished">No data to print.</translation>
+      <location filename="../printing.cpp" line="74"/>
+      <source>No data to print.</source>
+      <translation>لا توجد بيانات للطباعة.</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="82"/>
-        <source>Printing data.</source>
-        <translation type="unfinished">Printing data.</translation>
+      <location filename="../printing.cpp" line="82"/>
+      <source>Printing data.</source>
+      <translation>تتم طباعة البيانات.</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="108"/>
-        <source>Printing query.</source>
-        <translation type="unfinished">Printing query.</translation>
+      <location filename="../printing.cpp" line="108"/>
+      <source>Printing query.</source>
+      <translation>تتم طباعة الاستعلام.</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>PrintingExport</name>
     <message>
-        <location filename="../printingexport.cpp" line="27"/>
-        <source>Printing</source>
-        <translation type="unfinished">Printing</translation>
+      <location filename="../printingexport.cpp" line="27"/>
+      <source>Printing</source>
+      <translation>الطباعة</translation>
     </message>
-</context>
+  </context>
 </TS>
