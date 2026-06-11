5f132a1a0c305d45b801c4119b525b781fb2879b
Paweł Salawa <pawelsalawa@gmail.com>
2026-06-11 20:06:26 +0200

    New translations jsonexport.ts (Serbian (Cyrillic))
    [ci skip]

--- Plugins/JsonExport/translations/JsonExport_sr_SP.ts
+++ Plugins/JsonExport/translations/JsonExport_sr_SP.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sr" sourcelanguage="en">
-<context>
+  <context>
     <name>JsonExportConfig</name>
     <message>
-        <location filename="../jsonexport.ui" line="20"/>
-        <source>Output format</source>
-        <translation type="unfinished">Output format</translation>
+      <location filename="../jsonexport.ui" line="20"/>
+      <source>Output format</source>
+      <translation>Излазни формат</translation>
     </message>
     <message>
-        <location filename="../jsonexport.ui" line="26"/>
-        <source>Format document (new lines, indentation)</source>
-        <translation type="unfinished">Format document (new lines, indentation)</translation>
+      <location filename="../jsonexport.ui" line="26"/>
+      <source>Format document (new lines, indentation)</source>
+      <translation>Форматирај документ (нови редови, увлачење)</translation>
     </message>
     <message>
-        <location filename="../jsonexport.ui" line="42"/>
-        <source>Compress (everything in one line)</source>
-        <translation type="unfinished">Compress (everything in one line)</translation>
+      <location filename="../jsonexport.ui" line="42"/>
+      <source>Compress (everything in one line)</source>
+      <translation>Компресуј (све у једном реду)</translation>
     </message>
-</context>
+  </context>
 </TS>
