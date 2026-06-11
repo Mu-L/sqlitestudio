f312995ac803236ab179ae9127815a156d5b0b5e
Paweł Salawa <pawelsalawa@gmail.com>
2026-06-11 14:34:04 +0200

    New translations dbsqlitemc.ts (Greek)
    [ci skip]

--- Plugins/DbSQLite3MC/translations/DbSqliteMc_el_GR.ts
+++ Plugins/DbSQLite3MC/translations/DbSqliteMc_el_GR.ts
@@ -1,46 +1,46 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="el" sourcelanguage="en">
-<context>
+  <context>
     <name>DbSqliteMc</name>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="42"/>
-        <source>Password (key)</source>
-        <translation type="unfinished">Password (key)</translation>
+      <location filename="../dbsqlitemc.cpp" line="42"/>
+      <source>Password (key)</source>
+      <translation>Κωδικός πρόσβασης (κλειδί)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="43"/>
-        <source>Leave empty to create or connect to decrypted database.</source>
-        <translation type="unfinished">Leave empty to create or connect to decrypted database.</translation>
+      <location filename="../dbsqlitemc.cpp" line="43"/>
+      <source>Leave empty to create or connect to decrypted database.</source>
+      <translation>Αφήστε το κενό για να δημιουργήσετε ή να συνδεθείτε σε αποκρυπτογραφημένη βάση δεδομένων.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="44"/>
-        <source>Encryption password</source>
-        <translation type="unfinished">Encryption password</translation>
+      <location filename="../dbsqlitemc.cpp" line="44"/>
+      <source>Encryption password</source>
+      <translation>Κωδικός πρόσβασης κρυπτογράφησης</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="50"/>
-        <source>Cipher</source>
-        <translation type="unfinished">Cipher</translation>
+      <location filename="../dbsqlitemc.cpp" line="50"/>
+      <source>Cipher</source>
+      <translation>Κρυπτογραφικός αλγόριθμος</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="51"/>
-        <source>Cipher determines encryption algorithm used to encrypt the database.</source>
-        <translation type="unfinished">Cipher determines encryption algorithm used to encrypt the database.</translation>
+      <location filename="../dbsqlitemc.cpp" line="51"/>
+      <source>Cipher determines encryption algorithm used to encrypt the database.</source>
+      <translation>Ο κρυπτογραφικός αλγόριθμος καθορίζει τον αλγόριθμο κρυπτογράφησης που χρησιμοποιείται για την κρυπτογράφηση της βάσης δεδομένων.</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="61"/>
-        <source>Cipher configuration (optional)</source>
-        <translation type="unfinished">Cipher configuration (optional)</translation>
+      <location filename="../dbsqlitemc.cpp" line="61"/>
+      <source>Cipher configuration (optional)</source>
+      <translation>Ρύθμιση κρυπτογραφικού αλγορίθμου (προαιρετικό)</translation>
     </message>
     <message>
-        <location filename="../dbsqlitemc.cpp" line="62"/>
-        <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
+      <location filename="../dbsqlitemc.cpp" line="62"/>
+      <source>PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
 They will be executed upon each opening of the database.
 See documentation for SQLite3 Multiple Ciphers for details.</source>
-        <translation type="unfinished">PRAGMA statements to customize SQLite3 Multiple Ciphers configuration, such as KDF iterations, legacy mode, etc.
-They will be executed upon each opening of the database.
-See documentation for SQLite3 Multiple Ciphers for details.</translation>
+      <translation>Δηλώσεις PRAGMA για την προσαρμογή της διαμόρφωσης του SQLite3 Multiple Ciphers, όπως επαναλήψεις KDF, λειτουργία παλαιού τύπου κ.λπ.
+Θα εκτελούνται σε κάθε άνοιγμα της βάσης δεδομένων.
+Ανατρέξτε στην τεκμηρίωση του SQLite3 Multiple Ciphers για λεπτομέρειες.</translation>
     </message>
-</context>
+  </context>
 </TS>
