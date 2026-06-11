7acb20865fe86027700a22a72b3695a33d340e6d
Paweł Salawa <pawelsalawa@gmail.com>
2026-06-11 20:07:06 +0200

    New translations printing.ts (Belarusian)
    [ci skip]

--- Plugins/Printing/translations/Printing_be_BY.ts
+++ Plugins/Printing/translations/Printing_be_BY.ts
@@ -1,40 +1,40 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="be" sourcelanguage="en">
-<context>
+  <context>
     <name>Printing</name>
     <message>
-        <location filename="../printing.cpp" line="35"/>
-        <source>Print data</source>
-        <translation type="unfinished">Print data</translation>
+      <location filename="../printing.cpp" line="35"/>
+      <source>Print data</source>
+      <translation>Друкаваць даныя</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="37"/>
-        <source>Print query</source>
-        <translation type="unfinished">Print query</translation>
+      <location filename="../printing.cpp" line="37"/>
+      <source>Print query</source>
+      <translation>Друкаваць запыт</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="74"/>
-        <source>No data to print.</source>
-        <translation type="unfinished">No data to print.</translation>
+      <location filename="../printing.cpp" line="74"/>
+      <source>No data to print.</source>
+      <translation>Няма даных для друку.</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="82"/>
-        <source>Printing data.</source>
-        <translation type="unfinished">Printing data.</translation>
+      <location filename="../printing.cpp" line="82"/>
+      <source>Printing data.</source>
+      <translation>Друкаванне даных.</translation>
     </message>
     <message>
-        <location filename="../printing.cpp" line="108"/>
-        <source>Printing query.</source>
-        <translation type="unfinished">Printing query.</translation>
+      <location filename="../printing.cpp" line="108"/>
+      <source>Printing query.</source>
+      <translation>Друкаванне запыту.</translation>
     </message>
-</context>
-<context>
+  </context>
+  <context>
     <name>PrintingExport</name>
     <message>
-        <location filename="../printingexport.cpp" line="27"/>
-        <source>Printing</source>
-        <translation type="unfinished">Printing</translation>
+      <location filename="../printingexport.cpp" line="27"/>
+      <source>Printing</source>
+      <translation>Друк</translation>
     </message>
-</context>
+  </context>
 </TS>
