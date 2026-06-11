a15de3ade5b8e2f998dfae40d02765c661804c24
Paweł Salawa <pawelsalawa@gmail.com>
2026-06-11 17:49:31 +0200

    New translations letos.ts (Arabic)
    [ci skip]

--- Letos/letos/translations/letos_ar_SA.ts
+++ Letos/letos/translations/letos_ar_SA.ts
@@ -1,97 +1,97 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="ar" sourcelanguage="en">
-<context>
+  <context>
     <name>QObject</name>
     <message>
-        <location filename="../main.cpp" line="52"/>
-        <source>GUI interface to Letos, a SQLite manager.</source>
-        <translation type="unfinished">GUI interface to Letos, a SQLite manager.</translation>
+      <location filename="../main.cpp" line="52"/>
+      <source>GUI interface to Letos, a SQLite manager.</source>
+      <translation>واجهة رسومية إلى Letos، مدير SQLite.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="57"/>
-        <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
-        <translation type="unfinished">Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</translation>
+      <location filename="../main.cpp" line="57"/>
+      <source>Starts the application in safe mode without restoring the previous session. Use this to bypass issues caused by a corrupted session.</source>
+      <translation>يشغّل التطبيق في الوضع الآمن دون استعادة الجلسة السابقة. استخدم هذا لتجاوز المشكلات الناجمة عن جلسة تالفة.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="58"/>
-        <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
-        <translation type="unfinished">Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</translation>
+      <location filename="../main.cpp" line="58"/>
+      <source>Do not load any plugins. Can be used alongside safe mode to further isolate potential issues.</source>
+      <translation>لا تحمّل أي إضافات. يمكن استخدامه إلى جانب الوضع الآمن لعزل المشكلات المحتملة بشكل أكبر.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="59"/>
-        <source>Enables debug messages in console (accessible with F12).</source>
-        <translation type="unfinished">Enables debug messages in console (accessible with F12).</translation>
+      <location filename="../main.cpp" line="59"/>
+      <source>Enables debug messages in console (accessible with F12).</source>
+      <translation>يمكّن رسائل تصحيح الأخطاء في وحدة التحكم (يمكن الوصول إليها باستخدام F12).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="60"/>
-        <source>Redirects debug messages into standard output (forces debug mode).</source>
-        <translation type="unfinished">Redirects debug messages into standard output (forces debug mode).</translation>
+      <location filename="../main.cpp" line="60"/>
+      <source>Redirects debug messages into standard output (forces debug mode).</source>
+      <translation>يعيد توجيه رسائل تصحيح الأخطاء إلى الإخراج القياسي (يفرض وضع تصحيح الأخطاء).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>Redirects debug messages into given file (forces debug mode).</source>
-        <translation type="unfinished">Redirects debug messages into given file (forces debug mode).</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>Redirects debug messages into given file (forces debug mode).</source>
+      <translation>يعيد توجيه رسائل تصحيح الأخطاء إلى الملف المحدد (يفرض وضع تصحيح الأخطاء).</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="61"/>
-        <source>log file</source>
-        <translation type="unfinished">log file</translation>
+      <location filename="../main.cpp" line="61"/>
+      <source>log file</source>
+      <translation>ملف السجل</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="62"/>
-        <source>Enables Lemon parser debug messages for SQL code assistant.</source>
-        <translation type="unfinished">Enables Lemon parser debug messages for SQL code assistant.</translation>
+      <location filename="../main.cpp" line="62"/>
+      <source>Enables Lemon parser debug messages for SQL code assistant.</source>
+      <translation>يمكّن رسائل تصحيح الأخطاء لمحلّل Lemon لمساعد شيفرة SQL.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="63"/>
-        <source>Enables debugging of every single SQL query being sent to any database.</source>
-        <translation type="unfinished">Enables debugging of every single SQL query being sent to any database.</translation>
+      <location filename="../main.cpp" line="63"/>
+      <source>Enables debugging of every single SQL query being sent to any database.</source>
+      <translation>يمكّن تصحيح أخطاء كل استعلام SQL يُرسل إلى أي قاعدة بيانات.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
-        <translation type="unfinished">Limits SQL query messages to only the given &lt;database&gt;.</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>Limits SQL query messages to only the given &lt;database&gt;.</source>
+      <translation>يقصر رسائل استعلامات SQL على &lt;قاعدة البيانات&gt; المحددة فقط.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="64"/>
-        <source>database</source>
-        <translation type="unfinished">database</translation>
+      <location filename="../main.cpp" line="64"/>
+      <source>database</source>
+      <translation>قاعدة البيانات</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
-        <translation type="unfinished">Points to the master configuration file. Read manual at wiki page for more details.</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>Points to the master configuration file. Read manual at wiki page for more details.</source>
+      <translation>يشير إلى ملف التكوين الرئيسي. اقرأ الدليل في صفحة الويكي لمزيد من التفاصيل.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="56"/>
-        <source>settings file</source>
-        <translation type="unfinished">settings file</translation>
+      <location filename="../main.cpp" line="56"/>
+      <source>settings file</source>
+      <translation>ملف الإعدادات</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="65"/>
-        <source>Enables debugging of Letos&apos;s query executor.</source>
-        <translation type="unfinished">Enables debugging of Letos&apos;s query executor.</translation>
+      <location filename="../main.cpp" line="65"/>
+      <source>Enables debugging of Letos&apos;s query executor.</source>
+      <translation>يمكّن تصحيح أخطاء منفّذ الاستعلامات في Letos&apos;s.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="66"/>
-        <source>Lists plugins installed in the Letos and quits.</source>
-        <translation type="unfinished">Lists plugins installed in the Letos and quits.</translation>
+      <location filename="../main.cpp" line="66"/>
+      <source>Lists plugins installed in the Letos and quits.</source>
+      <translation>يعرض الإضافات المثبتة في Letos ثم يخرج.</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>file</source>
-        <translation type="unfinished">file</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>file</source>
+      <translation>ملف</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="79"/>
-        <source>Database file to open</source>
-        <translation type="unfinished">Database file to open</translation>
+      <location filename="../main.cpp" line="79"/>
+      <source>Database file to open</source>
+      <translation>ملف قاعدة بيانات لفتحه</translation>
     </message>
     <message>
-        <location filename="../main.cpp" line="152"/>
-        <source>Select configuration directory</source>
-        <translation type="unfinished">Select configuration directory</translation>
+      <location filename="../main.cpp" line="152"/>
+      <source>Select configuration directory</source>
+      <translation>تحديد دليل التكوين</translation>
     </message>
-</context>
+  </context>
 </TS>
