05ad6b809d0b7a7d78d31572def53c9bbd9271d0
Paweł Salawa <pawelsalawa@gmail.com>
2026-08-12 00:46:25 +0200

    New translations erdeditor.ts (Japanese)
    [ci skip]

--- Plugins/ErdEditor/translations/ErdEditor_ja_JP.ts
+++ Plugins/ErdEditor/translations/ErdEditor_ja_JP.ts
@@ -70,12 +70,12 @@
     <message>
       <location filename="../erdconfig.ui" line="69"/>
       <source>Highlight entities directly related to the selected entity and the relationships connecting them.</source>
-      <translation type="unfinished">Highlight entities directly related to the selected entity and the relationships connecting them.</translation>
+      <translation>選択したエンティティに直接関連するエンティティと、それらを接続するリレーションシップを強調表示します。</translation>
     </message>
     <message>
       <location filename="../erdconfig.ui" line="72"/>
       <source>Highlight related entities</source>
-      <translation type="unfinished">Highlight related entities</translation>
+      <translation>関連するエンティティを強調表示</translation>
     </message>
   </context>
   <context>
@@ -340,19 +340,19 @@
       <location filename="../erdwindow.cpp" line="268"/>
       <source>Pan view with Space only</source>
       <comment>ERD editor</comment>
-      <translation type="unfinished">Pan view with Space only</translation>
+      <translation>Space キーのみでビューをパン</translation>
     </message>
     <message>
       <location filename="../erdwindow.cpp" line="273"/>
       <source>Highlight related entities</source>
       <comment>ERD editor</comment>
-      <translation type="unfinished">Highlight related entities</translation>
+      <translation>関連するエンティティを強調表示</translation>
     </message>
     <message>
       <location filename="../erdwindow.cpp" line="284"/>
       <source>ERD settings</source>
       <comment>ERD editor</comment>
-      <translation type="unfinished">ERD settings</translation>
+      <translation>ERD 設定</translation>
     </message>
     <message>
       <location filename="../erdwindow.cpp" line="335"/>
