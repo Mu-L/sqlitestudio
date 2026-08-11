7e725bdc9c721b451d0977047e3eb8f9cdbac958
Paweł Salawa <pawelsalawa@gmail.com>
2026-08-12 00:46:18 +0200

    New translations erdeditor.ts (German)
    [ci skip]

--- Plugins/ErdEditor/translations/ErdEditor_de_DE.ts
+++ Plugins/ErdEditor/translations/ErdEditor_de_DE.ts
@@ -70,12 +70,12 @@
     <message>
       <location filename="../erdconfig.ui" line="69"/>
       <source>Highlight entities directly related to the selected entity and the relationships connecting them.</source>
-      <translation type="unfinished">Highlight entities directly related to the selected entity and the relationships connecting them.</translation>
+      <translation>Heben Sie Entitäten hervor, die direkt mit der ausgewählten Entität zusammenhängen, sowie die Beziehungen, die sie verbinden.</translation>
     </message>
     <message>
       <location filename="../erdconfig.ui" line="72"/>
       <source>Highlight related entities</source>
-      <translation type="unfinished">Highlight related entities</translation>
+      <translation>Zugehörige Entitäten hervorheben</translation>
     </message>
   </context>
   <context>
@@ -340,19 +340,19 @@
       <location filename="../erdwindow.cpp" line="268"/>
       <source>Pan view with Space only</source>
       <comment>ERD editor</comment>
-      <translation type="unfinished">Pan view with Space only</translation>
+      <translation>Ansicht nur mit Leertaste schwenken</translation>
     </message>
     <message>
       <location filename="../erdwindow.cpp" line="273"/>
       <source>Highlight related entities</source>
       <comment>ERD editor</comment>
-      <translation type="unfinished">Highlight related entities</translation>
+      <translation>Zugehörige Entitäten hervorheben</translation>
     </message>
     <message>
       <location filename="../erdwindow.cpp" line="284"/>
       <source>ERD settings</source>
       <comment>ERD editor</comment>
-      <translation type="unfinished">ERD settings</translation>
+      <translation>ERD-Einstellungen</translation>
     </message>
     <message>
       <location filename="../erdwindow.cpp" line="335"/>
