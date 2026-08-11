000da871830d68ede62ea14cb08ff2cef7d46891
Paweł Salawa <pawelsalawa@gmail.com>
2026-08-12 00:46:30 +0200

    New translations erdeditor.ts (Norwegian)
    [ci skip]

--- Plugins/ErdEditor/translations/ErdEditor_no_NO.ts
+++ Plugins/ErdEditor/translations/ErdEditor_no_NO.ts
@@ -70,12 +70,12 @@
     <message>
       <location filename="../erdconfig.ui" line="69"/>
       <source>Highlight entities directly related to the selected entity and the relationships connecting them.</source>
-      <translation type="unfinished">Highlight entities directly related to the selected entity and the relationships connecting them.</translation>
+      <translation>Uthev entiteter som er direkte relatert til den valgte entiteten, og relasjonene som forbinder dem.</translation>
     </message>
     <message>
       <location filename="../erdconfig.ui" line="72"/>
       <source>Highlight related entities</source>
-      <translation type="unfinished">Highlight related entities</translation>
+      <translation>Uthev relaterte entiteter</translation>
     </message>
   </context>
   <context>
@@ -340,19 +340,19 @@
       <location filename="../erdwindow.cpp" line="268"/>
       <source>Pan view with Space only</source>
       <comment>ERD editor</comment>
-      <translation type="unfinished">Pan view with Space only</translation>
+      <translation>Panorer visning bare med Mellomrom</translation>
     </message>
     <message>
       <location filename="../erdwindow.cpp" line="273"/>
       <source>Highlight related entities</source>
       <comment>ERD editor</comment>
-      <translation type="unfinished">Highlight related entities</translation>
+      <translation>Uthev relaterte entiteter</translation>
     </message>
     <message>
       <location filename="../erdwindow.cpp" line="284"/>
       <source>ERD settings</source>
       <comment>ERD editor</comment>
-      <translation type="unfinished">ERD settings</translation>
+      <translation>ERD-innstillinger</translation>
     </message>
     <message>
       <location filename="../erdwindow.cpp" line="335"/>
